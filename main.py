57b4eb63fadd632def1f40d10e6a0f9f9e5e941f
Artem <artem.kotyakhov@gmail.com>
2025-08-12 13:23:19 +0300

    feat: add date filter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 from collections import defaultdict
+from datetime import datetime
+
 from tabulate import tabulate
 
 
@@ -8,18 +10,23 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Обработка лог-файлов и формирование отчетов')
     parser.add_argument('--file', nargs='+', required=True, help='Путь до лог-файла')
     parser.add_argument('--report', choices=['average'], required=True, help='Тип отчета')
+    parser.add_argument('--date', help='Фильтрация лога по дате (YYYY-MM-DD)')
     return parser.parse_args()
 
 
-def read_logs(file_paths):
+def read_logs(file_paths, date_filter=None):
     logs = []
     for file_path in file_paths:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, 'r') as file:
             for line in file:
                 try:
                     log_entry = json.loads(line.strip())
+                    if date_filter:
+                        log_date = datetime.fromisoformat(log_entry['@timestamp']).date()
+                        if log_date != date_filter:
+                            continue
                     logs.append(log_entry)
-                except json.JSONDecodeError:
+                except (json.JSONDecodeError, KeyError):
                     continue
     return logs
 
@@ -57,7 +64,15 @@ def print_report(report_data):
 def main():
     args = parse_args()
 
-    logs = read_logs(args.file)
+    date_filter = None
+    if args.date:
+        try:
+            date_filter = datetime.strptime(args.date, '%Y-%m-%d').date()
+        except ValueError:
+            print("Неправильный формат даты. Ожидается: YYYY-MM-DD")
+            return
+
+    logs = read_logs(args.file, date_filter)
 
     if args.report == 'average':
         report = generate_average_report(logs)
